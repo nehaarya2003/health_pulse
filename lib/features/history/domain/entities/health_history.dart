14575dc70709937fd373835d5bfec42ccd5562bc
nehaarya2003 <nehaarya2003@gmail.com>
2026-09-21 16:30:14 +0530

    working on health dashborad part 7

---
+++ lib/features/history/domain/entities/health_history.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+import '../../dashboard/domain/entities/health_metric.dart';
+
+class HealthHistory extends Equatable {
+  final MetricType type;
+  final List<HealthReading> readings;
+
+  const HealthHistory({
+    required this.type,
+    required this.readings,
+  });
+
+  double get average {
+    if (readings.isEmpty) return 0;
+    return readings.map((r) => r.value).reduce((a, b) => a + b) /
+        readings.length;
+  }
+
+  double get max {
+    if (readings.isEmpty) return 0;
+    return readings.map((r) => r.value).reduce((a, b) => a > b ? a : b);
+  }
+
+  double get min {
+    if (readings.isEmpty) return 0;
+    return readings.map((r) => r.value).reduce((a, b) => a < b ? a : b);
+  }
+
+  @override
+  List<Object?> get props => [type, readings];
+}
+
